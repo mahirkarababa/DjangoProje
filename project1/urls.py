b0a2ca7d00ffec08af2916aa1f3c0eb7474cfda2
mahirkarababa <mahirkarababa09@gmail.com>
2020-04-28 18:34:56 +0300

    Ckeditor yüklendi

--- project1/urls.py
+++ project1/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('product/', include('product.urls')),
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
+    path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
