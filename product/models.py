380a554487dfc6742ab3159831b7ba5d2b41c3e1
mahirkarababa <mahirkarababa09@gmail.com>
2020-04-30 17:36:17 +0300

    Slidera ürün eklendi

--- product/models.py
+++ product/models.py
@@ -6,19 +6,20 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 class Category(models.Model):
 
-    STATUS =  (
+    STATUS = (
         ('True','Evet'),
         ('False','Hayır'),
     )
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
-    keywords = models.CharField(max_length=255)
+    description = models.CharField(blank=True,max_length=255)
+    keywords = models.CharField(blank=True,max_length=255)
     image = models.ImageField(blank=True,upload_to='images/')
     status = models.CharField(max_length=10,choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True)
     parent = models.ForeignKey('self',blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.title
@@ -26,6 +27,7 @@ class Category(models.Model):
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
+
 class Product(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -33,14 +35,17 @@ class Product(models.Model):
     )
     category = models.ForeignKey(Category, on_delete=models.CASCADE)  #relation with Category table
     title = models.CharField(max_length=150)
-    description = models.CharField(max_length=255)
-    keywords = models.CharField(max_length=255)
+    description = models.CharField(blank=True,max_length=255)
+    keywords = models.CharField(blank=True,max_length=255)
+    slug = models.SlugField(blank=True,max_length=150)
     image = models.ImageField(blank=True, upload_to='images/')
     salary = models.FloatField()
     detail = RichTextUploadingField()
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+
 
     def __str__(self):
         return self.title
