f8b47443208a2e9dde901741c1869aef7f847019
mahirkarababa <mahirkarababa09@gmail.com>
2020-05-05 17:04:42 +0300

    Üye Paneli Profil Bilgileri Yapıldı

--- product/models.py
+++ product/models.py
@@ -84,6 +84,7 @@ class Images(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     def __str__(self):
         return self.title
+
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
