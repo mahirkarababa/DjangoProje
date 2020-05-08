fe52a86ef0948bdaacc1ba638183cd646d6400da
mahirkarababa <mahirkarababa09@gmail.com>
2020-05-08 18:30:58 +0300

    Başvuru Tamamlama Yapıldı

--- order/urls.py
+++ order/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # ex: /home/
     path('', views.index, name='index'),
     path('addtocart/<int:id>', views.addtocart, name='addtocart'),
+    path('orderproduct/', views.orderproduct, name='orderproduct'),
     path('deletefromcart/<int:id>', views.deletefromcart, name='deletefromcart'),
     # ex: /polls/5/
     #path('<int:question_id>/', views.detail, name='detail'),
